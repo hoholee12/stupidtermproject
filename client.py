0afb44ed46df9881d08e18a9351ab4d4d5863673
yop yop yop <hoholee12@naver.com>
2015-10-23 10:09:14 +0900

    test

--- client.py
+++ client.py
@@ -1,4 +1,13 @@
 from socket import *
+from threading import *
+
+def clientrecv():
+	while 1:
+		servermsg=clientsocket.recv(1024)
+		print("server: "+servermsg.decode("utf8"))
+	clientsocket.close()
+	
+
 
 servername='127.0.0.1'
 serverport=7000
@@ -6,8 +15,11 @@ serverport=7000
 clientsocket=socket(AF_INET, SOCK_STREAM) #create socket
 clientsocket.connect((servername, serverport)) #connect => server.accept
 print("connected.");
+
+Thread(target=clientrecv).start()
+
 while 1:
-	rawdata=input('input string: ')
+	rawdata=input('')
 	clientsocket.send(rawdata.encode("utf8")) #send
-	rawdata=clientsocket.recv(1024) #receive
-	print("from server: ", rawdata)
+	#rawdata=clientsocket.recv(1024) #receive
+	#print("from server: ", rawdata)
--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 from socket import *
+from threading import *
+
+def serverrecv():
+	while 1:
+		clientmsg=connectionsocket.recv(1024)
+		print("client: "+clientmsg.decode("utf8"))
+
 
 servername=''
 serverport=7000
@@ -8,10 +15,16 @@ serversocket=socket(AF_INET, SOCK_STREAM) #create socket
 serversocket.bind((servername, serverport)) #bind address and socket
 
 serversocket.listen(1) #listen
-
+connectionsocket,addr=serversocket.accept() #accept and create connection socket
+print("client connected!\n", addr)
+	
+Thread(target=serverrecv).start()
+	
 while 1:
-	connectionsocket,addr=serversocket.accept() #accept and create connection socket
-	print("client connected! ", addr)
-	rawdata=connectionsocket.recv(1024) #receive
-	print(rawdata)
-	connectionsocket.send(rawdata.upper()) #send
+	#rawdata=connectionsocket.recv(1024) #receive
+	#print(rawdata)
+	#connectionsocket.send(rawdata.upper()) #send
+	rawdata=input()
+	connectionsocket.send(rawdata.encode("utf8"))
+	
+connectionsocket.close()
