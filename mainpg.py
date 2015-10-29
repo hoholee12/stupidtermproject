29d5d4aad03ed18a1053c73ddf451031ebc65b3b
yop yop yop <hoholee12@naver.com>
2015-10-29 09:08:04 +0900

    rewrite

--- mainpg.py
+++ mainpg.py
@@ -1,67 +1,39 @@
 #!/cygdrive/c/Python34/python.exe
 import pygame
-
 pygame.init()
 
-#assign them as tuple
-white=(255, 255, 255)
-black=(0, 0, 0)
-red=(255, 0, 0) #optional parameter alpha(transparency, opaque)
+'''
+4bytes 32bit hex: 0x0~0xffffffff(8) dec: 0~4294967295
+2bytes 16bit hex: 0x0~0xffff(4) dec: 0~65535
+1byte 8bit hex: 0x0~0xff(2) dec: 0~255
+'''
 
-clock=pygame.time.Clock() #returns clock object
+black=(0x0, 0x0, 0x0)
+white=(0xff, 0xff, 0xff)
+red=(0xff, 0x0, 0x0)
+green=(0x0, 0xff, 0x0)
+blue=(0x0, 0x0, 0xff)
 
+pi=3.141592653
 
-game_display=pygame.display.set_mode((640, 480)) #need to return one tuple, not two function parameters!
-pygame.display.set_caption('slither') #title for pygame top bar
-#pygame.display.flip() #update entire screen, almost never used xDD
+size=(700, 500)
+screen=pygame.display.set_mode(size)
 
-#pygame.display.update() #no parameters == flip() #not needed for now
+pygame.display.set_caption("mygame")
 
-#my game code here!
+done=False
 
-lead_x=300
-lead_y=300
-lead_x_change=0
-lead_y_change=0
+clock=pygame.time.Clock() #for clock.tick(60)
 
-
-game_exit=0
-while not game_exit:
-	for event in pygame.event.get():
-		#print(event)
-		if event.type == pygame.QUIT:
-			game_exit=1
-		if event.type == pygame.KEYDOWN: #key pressed
-			if event.key == pygame.K_LEFT:
-				#lead_x-=10
-				lead_x_change=-1
-			if event.key == pygame.K_RIGHT:
-				#lead_x+=10
-				lead_x_change=1
-			if event.key == pygame.K_UP:
-				#lead_y-=10
-				lead_y_change=-1
-			if event.key == pygame.K_DOWN:
-				#lead_y+=10
-				lead_y_change=1
-				
-		if event.type == pygame.KEYUP: #key released
-			lead_x_change=0
-			lead_y_change=0
-		
+while not done:
+	for i in pygame.event.get():
+		if i.type == pygame.QUIT:	#close button
+			done=True
+			
+	screen.fill(white) #clear screen
 	
-	lead_x+=lead_x_change
-	lead_y+=lead_y_change
-
-	game_display.fill(white)
-	
-	#pygame.draw.rect(game_display, black, [320, 240, 10, 100]) #where to draw, what color, x y width height as list
-	#or
-	game_display.fill(red, rect=[lead_x, lead_y, 10, 10]) #this is much faster! gpu rendering
-	
-	pygame.display.update()
-
-	clock.tick(60) #60 fps
+	pygame.display.flip() #update frame
+	clock.tick(60) #60fps
 
 
 
@@ -69,6 +41,3 @@ while not game_exit:
 
 
 
-pygame.quit() #uninitialize pygame module
-
-quit() #exit python
