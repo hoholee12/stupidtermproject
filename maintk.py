f050bfd9828516a24a29d8af76b5153488c7e3f9
yop yop yop <hoholee12@naver.com>
2015-11-05 23:39:27 +0900

    test12.py crypto techniques revived!

--- maintk.py
+++ maintk.py
@@ -1,114 +1,325 @@
-#!/cygdrive/c/Python34/python.exe
-
 from tkinter import *
+from tkinter.filedialog import asksaveasfilename
+import threading
+import socket
+from getpass import getpass
+from sys import *
 
-app=Tk() #window
+#global vars
+username="me"
+connectionsocket=0
 
-#label
-'''
-label=Label(app, text="hello world!") #label widget
+#my crypto technique
+secretkey="badasscryptography"
 
-label.pack() #place label widget into window
-'''
-#button
-'''
-topframe=Frame(app)
-topframe.pack()
+encrypt_on=0
+decrypt_on=0
 
-bottomframe=Frame(app)
-bottomframe.pack(side=BOTTOM)
+def changecfg(set):
+	global encrypt_on
+	global decrypt_on
+	if set == 0: #decrypt switch
+		if encrypt_on == 0:
+			encrypt_on=1
+		else:
+			encrypt_on=0
+	if set == 1: #decrypt switch
+		if decrypt_on == 0:
+			decrypt_on=1
+		else:
+			decrypt_on=0
 
-button1=Button(topframe, text="Button 1", fg="red") #fg color
-button2=Button(topframe, text="Button 2", fg="blue") #fg color
-button3=Button(topframe, text="Button 3", fg="green") #fg color
-button4=Button(bottomframe, text="Button 4", fg="purple") #fg color
+def encrypt(result):
+	multiplication=0
+	p=0
+	for c in secretkey:
+		p+=1
+		multiplication=multiplication+(p*ord(str(c)))
+		
+	stuff="1337"
+	stuff=stuff+str(1337*multiplication)
+	stuff=stuff+","
+	for j in range(0,len(result)):
+		stuff=stuff+str(multiplication*ord(result[j]))
+		try:
+			if result[j+1]:
+				stuff=stuff+","
+		except IndexError:
+			pass
+			
+	return stuff
 
-button1.pack(side=LEFT)
-button2.pack(side=LEFT)
-button3.pack(side=LEFT)
-button4.pack(side=LEFT)
-'''
-#fill color
-'''
-one=Label(app, text="one", bg="red", fg="white")
-one.pack()
-two=Label(app, text="two", bg="green", fg="black")
-two.pack(fill=X) #will grow in the x direction if window size changes
-three=Label(app, text="three", bg="blue", fg="white")
-three.pack(side=LEFT, fill=BOTH, expand=TRUE) #will grow in both direction if window size changes
-'''
-
-#grid
-'''
-label1=Label(app, text="name")
-label2=Label(app, text="passwd")
-entry1=Entry(app) #line input
-entry2=Entry(app) #line input
-
-label1.grid(row=0, sticky=E) #no more pack(), column defaults to zero, top
-#grid sticky North East West South
-label2.grid(row=1, sticky=E) #bottom
-
-entry1.grid(row=0, column=1) #same row as label1
-entry2.grid(row=1, column=1) #same row as label2
-
-c=Checkbutton(app, text="keep me logged in") #little square check box
-c.grid(columnspan=2) #take two columns
-'''
-#event buttonclick
-'''
-def printname(event):
-	print("hello world!")
-
-#button1=Button(app, text="printname", command=printname)
-button1=Button(app, text="printname")
-button1.bind("<Button-1>", printname) #binds button click to function with events
-button1.pack()
-'''
-#event on frame
-'''
-def leftclick(event):
-	print("left")
-
-def middleclick(event):
-	print("middle")
-
-def rightclick(event):
-	print("right")
-
-frame=Frame(app, width=320, height=240)
-frame.bind("<Button-1>", leftclick)
-frame.bind("<Button-2>", middleclick)
-frame.bind("<Button-3>", rightclick)
-
-frame.pack()
-'''
-
-
-class mybuttons:
-	def __init__(self, master): #master gui
-		frame=Frame(master)
-		frame.pack() #blank window
-		self.printbutton=Button(frame, text="hello world!", command=self.printmessage)
-		self.printbutton.pack(side=LEFT) #with a button
-		self.quitbutton=Button(frame, text="bye", command=frame.quit) #frame.quit is built-in
-		self.quitbutton.pack(side=LEFT) #will come out on right because theres printbutton already on the left
-
-	def printmessage(self):
-		print("hello, world!")
-
-b=mybuttons(app) #app is now master
+def decrypt(stuff):
+	if not "1337" in stuff:
+		print("its not mine.") #exception
+		return stuff
+	i=stuff.replace('1337','')
+	i=i.split(',')
+	multiplication=0
+	p=0
+	for c in secretkey:
+		p+=1
+		multiplication=multiplication+(p*ord(str(c)))
+	if not str(multiplication*1337) in i[0]:
+		print("incorrect password!")
+		exit()
+	i.pop(0)
+	result=""
+	for j in i:
+		result=result+chr(int(int(j)/multiplication))
+		
+	return result
 
 
 
+#menu funcs
+
+#file
+def savehistory():
+	file_name=asksaveasfilename(title="save chat", filetypes=[('plain text', '*.txt'), ('any file', '*.*')])
+	try:
+		filehandle=open(file_name+".txt", "w")
+	except IOError:
+		print("failed to save!")
+		return
+	contents=main_body_text.get(1.0, END)
+	for line in contents:
+		filehandle.write(line)
+	filehandle.close()
+	
+def username_options_window(master):
+	top=Toplevel(master)
+	top.title("username option")
+	top.grab_set() #limit event to this window
+	Label(top, text="username:").grid(row=0)
+	name=Entry(top)
+	name.focus_set() #focus window
+	name.grid(row=0, column=1)
+	go=Button(top, text="modify", command=lambda:username_options_go(name.get(), top))
+	go.grid(row=1, column=0, columnspan=2, sticky=W+E) #center of first row
+	
+def username_options_go(name, window):
+	for letter in [name]:
+		if letter==" " or letter=="\n":
+			error_window(root, "bad username, no spaces allowed!")
+			return
+	global username
+	username=name
+	writetoscreen("modified username: "+name, "system") #write to text widget on screen
+	window.destroy()
+	
+def server_options_window(master):
+	top=Toplevel(master)
+	top.title("server port option")
+	top.grab_set() #limit event to this window
+	top.protocol("WM_DELETE_WINDOW", lambda:optiondelete(top))
+	Label(top, text="port: ").grid(row=0)
+	port=Entry(top)
+	port.grid(row=0, column=1)
+	port.focus_set() #focus window
+	go=Button(top, text="start", command=lambda:server_options_go(port.get(), top))
+	go.grid(row=1, column=1)
+	
+def server_options_go(port, window):
+	if options_sanitation(port): #sanitizer
+		window.destroy()
+		server(int(port)).start() #start server thread
+		
+def optiondelete(window):
+	connector.config(state=NORMAL) #enable connect button
+	window.destroy()
+	
+def exitchat(window):
+	if connectionsocket != 0:
+		connectionsocket.close()
+	if clienttype == 1:
+		serv.close()
+	window.destroy()
+	
+def client_options_window(master):
+	top=Toplevel(master)
+	top.title("client connection settings")
+	top.protocol("WM_DELETE_WINDOW", lambda: optiondelete(top))
+	top.grab_set() #limit event to this window
+	Label(top, text="server IP:").grid(row=0)
+	location=Entry(top)
+	location.grid(row=0, column=1)
+	location.focus_set() #focus window
+	Label(top, text="port:").grid(row=1)
+	port=Entry(top)
+	port.grid(row=1, column=1)
+	go=Button(top, text="start", command=lambda: client_options_go(location.get(), port.get(), top))
+	go.grid(row=2, column=1)
+	
+def client_options_go(dest, port, window):
+	if options_sanitation(port, dest): #sanitizer
+		window.destroy()
+		client(dest, int(port)).start() #start client thread
+		
+def options_sanitation(por, loc=""):
+	if not por.isnumeric():
+		error_window(root, "insert port number")
+		return False
+	if int(por) < 0 or 65555 < int(por):
+		error_window(root, "range must be between 0~65555")
+		return False
+	if loc != "":
+		if not ip_process(loc.split(".")):
+			error_window(root, "insert a proper IP address")
+			return False
+	return True
+	
+def ip_process(iparray):
+	if len(iparray) != 4:
+		return False
+	for ip in iparray:
+		if not ip.isnumeric():
+			return False
+		t=int(ip)
+		if t<0 or 255<t:
+			return False
+	return True
+
+def error_window(master, texty):
+	window=Toplevel(master)
+	window.title("error")
+	window.grab_set()
+	Label(window, text=texty).pack()
+	go=Button(window, text="ok", command=window.destroy)
+	go.pack()
+	go.focus_set() #focus window
+	
+def connects(clienttype):
+	connector.config(state=DISABLED)
+	if clienttype == 0:
+		client_options_window(root)
+	if clienttype == 1:
+		server_options_window(root)
+		
+def toone():
+	global clienttype
+	clienttype=0
+	
+def totwo():
+	global clienttype
+	clienttype=1
+	
+def processusertext(event):
+	data=text_input.get()
+	placetext(data)
+	text_input.delete(0, END)
+	
+def placetext(text):
+	writetoscreen(text, username)
+	netthrow(connectionsocket, text)
+	
+def writetoscreen(text, username=""):
+	main_body_text.config(state=NORMAL)
+	main_body_text.insert(END, '\n')
+	if username:
+		main_body_text.insert(END, "["+username+"]")
+	main_body_text.insert(END, text)
+	main_body_text.yview(END)
+	main_body_text.config(state=DISABLED)
+	
+def netthrow(conn, message):
+	try:
+		if encrypt_on == 1: #if encrypt switch is on
+			conn.send(encrypt(message).encode("utf8"))
+		else:
+			conn.send(message.encode("utf8"))
+	except socket.error:
+		writetoscreen("failed to send msg", "system")
+		
+def netcatch(conn):
+	try:
+		message=conn.recv(1024) #bigger buffer size to accept huge encrypted numbers
+		if decrypt_on == 1:
+			return decrypt(message.decode("utf8"))
+		else:
+			return message.decode("utf8")
+	except socket.error:
+		writetoscreen("failed to receive msg", "system")
+		
+class server(threading.Thread):
+	def __init__(self, port):
+		threading.Thread.__init__(self)
+		self.port=port
+	
+	def run(self):
+		global serv
+		serv=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		serv.bind(('', self.port))
+		serv.listen(1)
+		global connectionsocket
+		writetoscreen("waiting from port"+str(self.port)+"...", "system")
+		connectionsocket,addr=serv.accept()
+		writetoscreen("connected to"+str(addr[0]), "system")
+		threading.Thread(target=runner, args=(connectionsocket,str(addr[0]))).start()
+		
+class client(threading.Thread):
+	def __init__(self, host, port):
+		threading.Thread.__init__(self)
+		self.port=port
+		self.host=host
+	
+	def run(self):
+		global connectionsocket
+		connectionsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		connectionsocket.connect((self.host, self.port))
+		writetoscreen("connected to IP:"+self.host+"port:"+str(self.port), "system")
+		
+		threading.Thread(target=runner, args=(connectionsocket, self.host)).start()
+		
+def runner(conn, addr):
+	while 1:
+		data=netcatch(conn)
+		writetoscreen(data, addr)
+		
+root=Tk()
+root.title("GUI 채팅")
+
+menubar=Menu(root)
+root.config(menu=menubar)
+file_menu=Menu(menubar, tearoff=0) #tearoff=0 removes dotted line
+menubar.add_cascade(label="file", menu=file_menu)
+file_menu.add_command(label="save", command=lambda: savehistory())
+file_menu.add_command(label="username", command=lambda:username_options_window(root))
+file_menu.add_command(label="exit", command=lambda: exitchat(root))
+crypt_menu=Menu(menubar, tearoff=0) #tearoff=0 removes dotted line
+menubar.add_cascade(label="crypt", menu=crypt_menu)
+crypt_menu.add_command(label="encrypt output", command=lambda: changecfg(0))
+crypt_menu.add_command(label="decrypt input", command=lambda: changecfg(1))
 
 
 
+main_body=Frame(root) #main display
+#text left side
+main_body_text=Text(main_body)
+main_body_text.pack(side=LEFT, fill=Y)
+#scroller right side
+body_text_scroll=Scrollbar(main_body)
+body_text_scroll.pack(side=RIGHT, fill=Y)
+#bind them
+body_text_scroll.config(command=main_body_text.yview)
+main_body_text.config(yscrollcommand=body_text_scroll.set)
+main_body.pack()
 
+main_body_text.insert(END, "welcome")
+main_body_text.config(state=DISABLED) #disable text
 
+#bottom corner
+text_input=Entry(root, width=60)
+text_input.bind("<Return>", processusertext)
+text_input.pack()
 
+clienttype=1
+Radiobutton(root, text="client", variable=clienttype, value=0, command=toone).pack(anchor=E)
+Radiobutton(root, text="server", variable=clienttype, value=0, command=totwo).pack(anchor=E)
 
+statusconnect=StringVar()
+statusconnect.set("connect")
+connector=Button(root, textvariable=statusconnect, command=lambda: connects(clienttype))
+connector.pack()
 
-
-
-app.mainloop() #mainloop
+root.mainloop()
